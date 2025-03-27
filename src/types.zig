8b698f56dfdaac2d5e64b25757b1426ac26fc03f
Dysthesis <antheoraviel@protonmail.com>
2025-03-27 23:15:53 +1100

    Implement the basic structure of the VarINt type

---
+++ src/types.zig
@@ -0,0 +1,39 @@
+// Originally from:
+// https://github.com/regenerativep/zigmcp/blob/f3a5d84ed361120163e38399722ecf36c7f456b3/src/varint.zig
+
+/// This function takes in an arbitrary integer type `I` and returns a VarInt type that spans said
+/// integer type.
+///
+/// Variable-length integers such that smaller values occupies less memory.
+///
+/// These are encoded in 8-bit chunks, where
+///
+/// - the 7 least significant bits are used to encode the value, and
+/// - the most significant bit indicates whether there is another chunk after it for the next part
+///   of the number
+///
+/// `VarInt`s are little endian, but with 7-bit groups rather than 8.
+///
+/// `VarInt`s are never longer than 5 bytes.
+///
+/// `VarInt`s are generic over arbitrary integer types (`i8`, `i16``, etc.)
+pub fn VarInt(comptime I: type) type {
+    // Get the type information of the integer type we are using. This is necessary to construct
+    // our `VarInt` type to fit the integer type we are using.
+    const typeinfo = @typeInfo(I).Int;
+    return struct {
+        /// The unsigned version of I. This is because the encoding or decoding of `VarInt`s, by
+        /// definition, involves isolating 7 bits at a time. Using a signed type could result in
+        /// sign extensions during bit shifting; that is, extra 1 bits could appear on the left if
+        /// the number is negative.
+        pub const Unit = @Type(.Int{ .signedness = .unsigned, .bits = typeinfo.bits });
+
+        /// This represents the maximum number of 7-bit chunks are needed to represent an integer
+        /// with the bit width of `I`.
+        pub const MaxBytes = (typeinfo.bits + 6) / 7;
+
+        /// A `Chunk` of the `VarInt`, as per the specifications. The 7 least significant bits hold
+        /// data while the most significant bit determines whether there is another chunk after it.
+        pub const Chunk = if (typeinfo.bits < 8) I else u8;
+    };
+}
