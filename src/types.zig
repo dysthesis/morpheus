7f5dbe4db012ce22ea590171515c43106599127b
Dysthesis <antheoraviel@protonmail.com>
2025-03-27 23:34:33 +1100

    Implement reader method for VarInt

--- src/types.zig
+++ src/types.zig
@@ -21,6 +21,9 @@ pub fn VarInt(comptime I: type) type {
     // Get the type information of the integer type we are using. This is necessary to construct
     // our `VarInt` type to fit the integer type we are using.
     const typeinfo = @typeInfo(I).Int;
+
+    const CONTINUE_BITS = 0x80;
+
     return struct {
         /// The unsigned version of I. This is because the encoding or decoding of `VarInt`s, by
         /// definition, involves isolating 7 bits at a time. Using a signed type could result in
@@ -35,5 +38,31 @@ pub fn VarInt(comptime I: type) type {
         /// A `Chunk` of the `VarInt`, as per the specifications. The 7 least significant bits hold
         /// data while the most significant bit determines whether there is another chunk after it.
         pub const Chunk = if (typeinfo.bits < 8) I else u8;
+
+        // TODO: figure out a better type for reader.
+        // TODO: document this method.
+        pub fn read(reader: anytype, out: *I, _: anytype) !void {
+            var result: Unit = 0;
+
+            for (0..MaxBytes) |pos| {
+                const curr_chunk = try reader.readByte();
+
+                // Check if there is another chunk after this, but it won't fit.
+                const continues = ((curr_chunk & CONTINUE_BITS) != 0);
+                if (continues and pos == MaxBytes - 1) {
+                    return error.VarIntTooBig;
+                }
+
+                // TODO: document this part
+                result |= @as(Unit, @as(u7, @truncate(curr_chunk))) <<
+                    (7 * @as(std.math.Log2Int(Unit), @intCast(pos)));
+
+                if (!continues) break;
+            }
+
+            out.* = @bitCast(result);
+        }
     };
 }
+
+const std = @import("std");
