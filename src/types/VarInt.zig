8400d6e5c6f54268a8ce5d34b1834bf83a725d68
Dysthesis <antheoraviel@protonmail.com>
2025-03-29 15:15:58 +1100

    Refactor VarInt
    Includes a few optimisations

--- src/types/VarInt.zig
+++ src/types/VarInt.zig
@@ -1,4 +1,4 @@
-// Originally from:
+// Originally from
 // https://github.com/regenerativep/zigmcp/blob/f3a5d84ed361120163e38399722ecf36c7f456b3/src/varint.zig
 
 const std = @import("std");
@@ -28,6 +28,8 @@ pub fn VarInt(comptime I: type) type {
     // A bitmask to get the bit indicating if there is another chunk after this.
     const CONTINUE_BITS = 0x80;
 
+    const DATA_BITS_COUNT = 7;
+
     return struct {
         value: I,
         pub const Self = VarInt(I);
@@ -36,7 +38,7 @@ pub fn VarInt(comptime I: type) type {
         /// involves isolating 7 bits at a time. Using a signed type could result in sign extensions
         /// during bit shifting; that is, extra 1 bits could appear on the left if the number is
         /// negative.
-        pub const Unit = @Type(.Int{ .signedness = .unsigned, .bits = typeinfo.bits });
+        pub const UnsignedSelf = @Type(.Int{ .signedness = .unsigned, .bits = typeinfo.bits });
 
         /// This represents the maximum number of 7-bit chunks are needed to represent an integer
         /// with the bit width of `I`.
@@ -46,9 +48,9 @@ pub fn VarInt(comptime I: type) type {
         /// data while the most significant bit determines whether there is another chunk after it.
         pub const Chunk = if (typeinfo.bits < 8) I else u8;
 
-        /// Read in chunks and initialise a new instance of `VarInt`
-        pub fn fromBytes(reader: std.io.Reader(u8)) !Self {
-            const value: I = read(reader);
+        /// Initialise a new instance of `VarInt`
+        pub fn init(reader: std.io.Reader(u8)) !I {
+            const value: I = decode(reader);
             return Self{ .value = value };
         }
 
@@ -56,30 +58,81 @@ pub fn VarInt(comptime I: type) type {
         // parallel to the reading of chunks. See: https://github.com/as-com/varint-simd.
         // Try benchmarking this, and use SIMD if there is a bottleneck here.
 
+        /// Given a chunk, align its data segment to its appropriate position.
+        inline fn alignChunkData(chunk: u8, chunk_pos: u8) UnsignedSelf {
+            // Get the position of the data section of the current chunk in the resulting Unit
+            const data_pos = 7 * @as(std.math.Log2Int(UnsignedSelf), @intCast(chunk_pos));
+
+            // Get the value of the current chunk, and combine it with the `result`
+            const data = @as(UnsignedSelf, @as(u7, @truncate(chunk))) << data_pos;
+
+            return data;
+        }
+
+        /// Decodes the given chunk, returning the `continue` indicator and the correctly-aligned
+        /// data.
+        inline fn decodeChunk(chunk: u8, pos: u8) struct { bool, UnsignedSelf } {
+            const curr_chunk = chunk;
+            // Check if there is another chunk after this, but it won't fit.
+            const continues = ((curr_chunk & CONTINUE_BITS) != 0);
+            if (continues and pos == MaxBytes - 1) {
+                return error.VarIntTooBig;
+            }
+
+            const data = alignChunkData(curr_chunk, pos);
+            return .{ continues, data };
+        }
+
         /// Read in chunks and return the resulting value.
-        pub fn read(reader: std.io.Reader(u8)) !I {
-            var result: Unit = 0;
+        pub fn decode(reader: std.io.Reader(u8)) !I {
+            var result: UnsignedSelf = 0;
 
             for (0..MaxBytes) |pos| {
                 const curr_chunk = try reader.readByte();
+                const continues, const data = decodeChunk(curr_chunk, pos);
 
-                // Check if there is another chunk after this, but it won't fit.
-                const continues = ((curr_chunk & CONTINUE_BITS) != 0);
-                if (continues and pos == MaxBytes - 1) {
-                    return error.VarIntTooBig;
-                }
-
-                // Get the position of the data section of the current chunk (the 7 last bits).
-                const data_pos = 7 * @as(std.math.Log2Int(Unit), @intCast(pos));
-
-                // Get the value of the current chunk, and combine it with the `result`
-                const data = @as(Unit, @as(u7, @truncate(curr_chunk))) << data_pos;
                 result |= data;
-
                 if (!continues) break;
             }
 
             return @bitCast(result);
+        }
+
+        pub fn write(self: Self, writer: anytype) !void {
+            // Ensure that `writer` has the necessary methods
+            comptime {
+                _ = writer.writeByte;
+            }
+
+            var value: UnsignedSelf = @bitCast(self.value);
+            while (true) {
+                const data: u8 = @as(u7, @truncate(value));
+
+                // PERF: This is a branchless statement that is equivalent to the following
+                // conditional:
+                //
+                // if (typeinfo.bits < 8)
+                //     value = 0
+                // else
+                //     value >>= 7;
+                //
+                // This should be more efficient than its implementation with branching.
+                // See: https://en.algorithmica.org/hpc/pipelining/branching
+                value = (value >> 7) * @intFromBool(typeinfo.bits >= 8);
+
+                // PERF: Likewise here, although we can't completely eliminate branching, we can
+                // avoid its costs on as many code paths as we can. This code is equivalent to:
+                //
+                // if (value == 0) {
+                //     try writer.writeByte(data);
+                //     break;
+                // } else {
+                //     try writer.writeByte(data | 0b1000_0000);
+                // }
+                try writer.writeByte(data | @as(u8, @intFromBool(value != 0)) << DATA_BITS_COUNT);
+
+                if (value == 0) break;
+            }
         }
     };
 }
